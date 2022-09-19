336347fa958add9a679ca3526a939af393d36b8d
UgurKADIOGLU <ugrkadioglu@gmail.com>
2022-09-19 15:02:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    int sayi=20;
+        if(sayi<20){
+            System.out.println("Sayi 20 den küçüktür");
+        }else if (sayi==20){
+            System.out.println("Sayi 20 ye eşittir");
+        }
+
+        if(sayi<15){
+            System.out.println("Sayi 15 den küçüktür");
+        }
+    }
+}
